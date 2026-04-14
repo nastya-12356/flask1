ae13bbfa752e061f6439ef96fddd6633c09cdafc
anastasia <anastasiasvejova@.gmail.com>
2026-04-14 14:51:58 +0300

    сделала задачу личная карточка web 2

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ login_manager.init_app(app)
 answers = {
     'title': 'Анкета',
     'surname': 'Watny',
-    'name': 'Sonya',
+    'name': 'Mark',
     'education': 'выше среднего',
     'profession': 'штурман марсохода',
     'sex': 'male',
